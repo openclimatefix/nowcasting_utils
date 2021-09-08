15e2be192dfb407de81199f5727f90a6f1f615d4
Jacob Bieker <jacob@openclimatefix.org>
2021-09-08 15:42:06 +0100

    Add base model and HF integration

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+requirementPath = this_directory / 'requirements.txt'
+with open(requirementPath) as f:
+    install_requires = f.read().splitlines()
+
+
+setup(
+    name="nowcasting_utils",
+    packages=find_packages(),
+    version="0.0.1",
+    license="MIT",
+    description="Nowcasting Utilities",
+    author="Jacob Bieker, Jack Kelly, Peter Dudfield",
+    author_email="jacob@openclimatefix.org",
+    company="Open Climate Fix Ltd",
+    url="https://github.com/openclimatefix/nowcasting_utils",
+    keywords=[
+        "artificial intelligence",
+        "deep learning",
+        "transformer",
+    ],
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
