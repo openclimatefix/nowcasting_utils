2fbe4818fed4762186345ea82405cbfe05acd064
Jacob Bieker <jacob@openclimatefix.org>
2021-09-09 14:41:58 +0100

    Update Actions

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -1,6 +1,7 @@
 from nowcasting_utils.models.normalization import metnet_normalization
 import numpy as np
 
+
 def test_metnet_normalization():
     data = np.random.random((2, 24, 12, 256, 256))
     data *= 1000 # Ensure not already between -1 and 1
