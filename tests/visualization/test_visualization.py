957168a4a13521fa7669dffec12678ec88014e2d
peterdudfield <peter.dudfield@hotmail.com>
2021-11-01 17:04:23 +0000

    update visualization

--- tests/visualization/test_visualization.py
+++ tests/visualization/test_visualization.py
@@ -1,5 +1,7 @@
 from nowcasting_utils.visualization.visualization import plot_example
 from nowcasting_dataset.consts import NWP_VARIABLE_NAMES
+from nowcasting_dataset.config.model import Configuration
+from predict_pv_yield.data.dataloader import FakeDataset
 import torch
 
 import tilemapbase
@@ -7,44 +9,22 @@ import tilemapbase
 # for Github actions need to create this
 tilemapbase.init(create=True)
 
+from nowcasting_dataloader.batch import BatchML
+
 
 def get_batch(
-    batch_size=8,
-    seq_length_5=19,
-    seq_length_30=4,
-    width=64,
-    height=64,
-    number_sat_channels=len(NWP_VARIABLE_NAMES),
 ):
 
-    x = {
-        "sat_data": torch.randn(batch_size, seq_length_5, width, height, number_sat_channels),
-        "pv_yield": torch.randn(batch_size, seq_length_5, 128),
-        "pv_system_id": torch.randn(batch_size, 128),
-        "nwp": torch.randn(batch_size, 10, seq_length_5, 2, 2),
-        "hour_of_day_sin": torch.randn(batch_size, seq_length_5),
-        "hour_of_day_cos": torch.randn(batch_size, seq_length_5),
-        "day_of_year_sin": torch.randn(batch_size, seq_length_5),
-        "day_of_year_cos": torch.randn(batch_size, seq_length_5),
-        "gsp_yield": torch.randn(batch_size, seq_length_30, 32),
-        "gsp_id": torch.randn(batch_size, 32),
-    }
+    c = Configuration()
+    c.input_data = c.input_data.set_all_to_defaults()
 
-    # add a nan
-    x["pv_yield"][0, 0, :] = float("nan")
+    # set up fake data
+    train_dataset = FakeDataset(configuration=c)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=None)
 
-    # add fake x and y coords, and make sure they are sorted
-    x["sat_x_coords"], _ = torch.sort(torch.randn(batch_size, seq_length_5))
-    x["sat_y_coords"], _ = torch.sort(torch.randn(batch_size, seq_length_5), descending=True)
-    x["gsp_system_x_coords"], _ = torch.sort(torch.randn(batch_size, seq_length_30))
-    x["gsp_system_y_coords"], _ = torch.sort(
-        torch.randn(batch_size, seq_length_30), descending=True
-    )
-
-    # add sorted (fake) time series
-    x["sat_datetime_index"], _ = torch.sort(torch.randn(batch_size, seq_length_5))
-    x["nwp_target_time"], _ = torch.sort(torch.randn(batch_size, seq_length_5))
-    x["gsp_datetime_index"], _ = torch.sort(torch.randn(batch_size, seq_length_30))
+    # get data
+    x = next(iter(train_dataloader))
+    x = BatchML(**x)
 
     return x
 
