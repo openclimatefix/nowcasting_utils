b1e6ce7634adc35fe9a954d1e77930a2425237d8
peterdudfield <peter.dudfield@hotmail.com>
2021-11-16 08:05:37 +0000

    add comments to tests

--- tests/visualization/data_sources/test_satellite.py
+++ tests/visualization/data_sources/test_satellite.py
@@ -1,24 +1,21 @@
 """ Tests to plot satellite data """
 import os
 
-import numpy as np
 import plotly.graph_objects as go
 from nowcasting_dataset.data_sources.fake import satellite_fake
 from nowcasting_dataset.geospatial import osgb_to_lat_lon
-from plotly.subplots import make_subplots
 
 from nowcasting_utils.visualization.data_sources.plot_satellite import (
     make_animation_all_channels,
     make_animation_one_channels,
     make_traces_one_channel,
     make_traces_one_channel_one_time,
-    make_traces_one_timestep,
 )
-from nowcasting_utils.visualization.utils import make_buttons, make_slider
+from nowcasting_utils.visualization.utils import make_buttons
 
 
 def test_make_traces_one_channel_one_time():
-
+    """ Test 'make_traces_one_channel_one_time' functions """
     satellite = satellite_fake(
         batch_size=2, seq_length_5=5, satellite_image_size_pixels=32, number_satellite_channels=2
     )
@@ -44,7 +41,7 @@ def test_make_traces_one_channel_one_time():
 
 
 def test_make_traces_one_channel():
-
+    """ Test 'make_traces_one_channel' functions """
     satellite = satellite_fake(
         batch_size=2, seq_length_5=5, satellite_image_size_pixels=32, number_satellite_channels=2
     )
@@ -80,7 +77,7 @@ def test_make_traces_one_channel():
 
 
 def test_make_animation_one_channels():
-
+    """ Test 'make_animation_one_channels' functions """
     satellite = satellite_fake(
         batch_size=2, seq_length_5=5, satellite_image_size_pixels=32, number_satellite_channels=2
     )
@@ -92,7 +89,7 @@ def test_make_animation_one_channels():
 
 
 def test_make_animation_all_channesl():
-
+    """ Test 'make_animation_all_channels' functions """
     # import xarray as xr
     # satellite = xr.load_dataset('/Users/peterdudfield/Documents/Github/nowcasting_utils/tests/data/sat/000000.nc')
     # satellite = satellite.rename({'stacked_eumetsat_data':'data', "variable":"channels"})
