9692f83548e7cc188970640455b38aaf98c75d43
peterdudfield <peter.dudfield@hotmail.com>
2021-11-19 12:56:52 +0000

    fix merge conflicts

--- notebooks/plotting.py
+++ notebooks/plotting.py
@@ -1,36 +1,19 @@
-<<<<<<< HEAD
 """ PLotting script for real data """
-=======
-import plotly.graph_objects as go
->>>>>>> dfa01e1458c006ee47e9654b1ef64b07aec48645
 from nowcasting_dataset.dataset.batch import Batch
-from nowcasting_dataset.geospatial import osgb_to_lat_lon
 
 from nowcasting_utils.visualization.data_sources.plot_all import (
     make_fig_time_series_pv_and_gsp,
-    make_satellite_gsp_pv_map,
     make_satellite_gsp_pv_map_one_time_value,
-<<<<<<< HEAD
     make_satellite_gsp_pv_map,
 )
-
 
 import plotly.graph_objects as go
 from nowcasting_dataset.geospatial import osgb_to_lat_lon
 
-from nowcasting_utils.visualization.data_sources.plot_gsp import (
-    get_fig_gsp_combined,
-=======
->>>>>>> dfa01e1458c006ee47e9654b1ef64b07aec48645
-)
 from nowcasting_utils.visualization.data_sources.plot_gsp import get_fig_gsp_combined
 from nowcasting_utils.visualization.data_sources.plot_pv import get_fig_pv_combined
 from nowcasting_utils.visualization.data_sources.plot_satellite import make_animation_all_channels
 
-<<<<<<< HEAD
-
-=======
->>>>>>> dfa01e1458c006ee47e9654b1ef64b07aec48645
 # load batch
 file_path = "/Users/peterdudfield/Documents/Github/nowcasting_utils/train"
 batch = Batch.load_netcdf(file_path, 0)
@@ -106,10 +89,7 @@ def plot_make_satellite_gsp_pv_map_one_time_step(batch):
 
 
 def plot_make_satellite_gsp_pv_map(batch):
-<<<<<<< HEAD
     """ Animate satellite, pv and gsp """
-=======
->>>>>>> dfa01e1458c006ee47e9654b1ef64b07aec48645
     fig = make_satellite_gsp_pv_map(batch=batch, example_index=15, satellite_channel_index=7)
 
     fig.show(renderer="browser")
