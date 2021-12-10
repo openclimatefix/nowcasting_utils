e9ec4f9bb46e297190d135cb6303188b5ce193aa
peterdudfield <peter.dudfield@hotmail.com>
2021-12-10 10:40:28 +0000

    add script

---
+++ scripts/plot-pv-and-gsp.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import xarray as xr
+import fsspec
+import io
+import json
+import plotly.graph_objects as go
+from nowcasting_dataset.data_sources.gsp.eso import get_gsp_shape_from_eso, get_gsp_metadata_from_eso
+
+# PV file
+pv_metadata_fileanme = "gs://solar-pv-nowcasting-data/PV/Passive/ocf_formatted/v0/system_metadata.csv"
+pv_filename = "gs://solar-pv-nowcasting-data/PV/Passive/ocf_formatted/v0/passiv.netcdf"
+
+pv_metadata = pd.read_csv(pv_metadata_fileanme)
+with fsspec.open(pv_filename, mode="rb") as file:
+    file_bytes = file.read()
+
+with io.BytesIO(file_bytes) as file:
+    pv_power = xr.open_dataset(file, engine="h5netcdf")
+    print(pv_power)
+
+
+ids = list(pv_power.keys())
+ids = [int(x) for x in ids]
+pv = pv_metadata[pv_metadata.ss_id.isin(ids)]
+
+## ESO
+gsp_shape = get_gsp_shape_from_eso().to_crs("EPSG:4326")
+# set z axis
+gsp_shape["Amount"] = 0
+
+# optional to only plot some gsp ids
+gsp_metadata = get_gsp_metadata_from_eso()
+gsp_id =[261,170,332,280,228,255,162,158]
+gsp_metadata = gsp_metadata[gsp_metadata['gsp_id'].isin(gsp_id)]
+gsp_shape = gsp_shape[gsp_shape.RegionID.isin(gsp_metadata.region_id)]
+
+# get dict shapes
+shapes_dict = json.loads(gsp_shape.to_json())
+
+trace_gsp = go.Choroplethmapbox(
+        geojson=shapes_dict, locations=gsp_shape.index, z=gsp_metadata.Amount, colorscale="Viridis", marker=dict(opacity=0.5)
+    )
+
+
+trace_pv = go.Scattermapbox(
+        lat=pv.latitude,
+        lon=pv.longitude,
+        marker=dict(color="Red", size=3, sizemode="area"),
+        name='PV',
+        text=pv.system_id,
+    )
+
+fig = go.Figure(
+        data=[trace_gsp, trace_pv],
+        layout=go.Layout(
+            title="PV systems",
+        ),
+    )
+fig.update_layout(
+    mapbox_style="carto-positron", mapbox_zoom=4, mapbox_center={"lat": 54, "lon": -1}
+)
+
+fig.show(renderer='browser')
